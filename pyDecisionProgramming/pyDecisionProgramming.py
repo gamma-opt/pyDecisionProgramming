c01cda2a6e53732e0ac99c4d196f5e5abacf0307
Jarno Rantaharju <rantahar@gmail.com>
2021-04-30 20:11:09 +0300

    Add functions for calling States

--- pyDecisionProgramming/pyDecisionProgramming.py
+++ pyDecisionProgramming/pyDecisionProgramming.py
@@ -1,5 +1,6 @@
 from julia import Pkg
 from julia import Main
+from julia import DecisionProgramming as jdp
 
 
 def setup_project():
@@ -13,3 +14,28 @@ def setup_project():
     Pkg.add(url=github_url)
     Pkg.add("Gurobi")
     Pkg.add("JuMP")
+
+
+def states(state_list):
+    """ Set states in the graph
+
+    state_list -- formatted as [(n, id)], where n is the
+    number of states and id an iteger identifying the node
+    """
+
+    params = [(c, [n]) for c, n in state_list]
+
+    graph_states = jdp.States(params)
+    return graph_states
+
+
+def create_states(ids, state_counts):
+    """ Set states in the graph
+
+    ids -- a list of numbers identifying each node
+    state_counts -- list of the number of states on each node
+    """
+
+    params = [(c, [n]) for c, n in zip(state_counts, ids)]
+    graph_states = states(params)
+    return graph_states
