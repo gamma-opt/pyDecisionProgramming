95b1ab384674efad2d6a09ed4d70f96159607bc3
Jarno Rantaharju <rantahar@gmail.com>
2021-05-03 17:13:10 +0300

    Add a wrapper for the Probabilities-type and for Julia Vector
    The Probabilities type does get's turned into a list instead of just 
    wrapping when returned to Python. We wrap it with a custom class to 
    prevent this.
    
    This also requires wrapping the Vector class.

--- pyDecisionProgramming/pyDecisionProgramming.py
+++ pyDecisionProgramming/pyDecisionProgramming.py
@@ -32,6 +32,90 @@ def setupProject():
     Main.eval('using JuMP')
 
 
+class Probabilities:
+    ''' A wrapper for the DecisionProgramming.jl probabilities
+    class.
+
+    Without the wrapper it get's interpreted as a python array,
+    which in turn becomes a Julia vector.
+
+    self.id -- The node id the probabilies correspond to
+    self.name -- The name of the Probabilities-variable in
+        Julia main name space.
+    '''
+
+    def __init__(self, id, probabilities):
+        ''' Set propabilities for outcomes on a chance node and
+        build the corresponding probabilities object.
+
+        id -- The id of the node
+        probabilities -- List of probabilities for each outcome
+        '''
+
+        self.id = id
+        self.name = 'n'+uuid.uuid4().hex[:8]
+        print('prob name', self.name)
+        Main.pDR_id = id
+        Main.pDR_p = probabilities
+        Main.eval(f'{self.name} = Probabilities(pDR_id, pDR_p)')
+
+    def __str__(self):
+        return getattr(Main, self.name)
+
+
+class Vector:
+    ''' Corresponds to the Julia Vector class.
+
+    We implement this here since the python Array class does
+    not automatically support the DecisionProgramming types
+    defined on the Julia side.
+    '''
+
+    def __init__(self, type: str):
+        ''' Create a new Julia vector of a given type
+
+        type: str -- The Julia type the vector contains
+        '''
+
+        # Generate a unique 9 character name
+        self.name = 'n'+uuid.uuid4().hex[:8]
+        print('vector name', self.name)
+        self.type = type
+
+        print(f'{self.name} = Vector{{{type}}}()')
+        Main.eval(f'{self.name} = Vector{{{type}}}()')
+
+    def __str__(self):
+        return getattr(Main, self.name).__str__()
+
+    def setName(self, name):
+        Main.eval(f'{name} = {self.name}')
+        self.name = name
+
+    def push(self, element):
+        ''' Push and element to a given vector
+
+        element -- the new element
+        '''
+
+        if self.type == 'Probabilities':
+            assert(isinstance(element, Probabilities))
+
+            print(f'push!(Main.{self.name}, Main.{element.name})')
+            Main.eval(
+                f'push!(Main.{self.name}, Main.{element.name})'
+            )
+            return
+
+        Main.pDR_e = element
+        print(Main.eval('typeof(Main.pDR_e)'))
+        print(Main.eval('Main.pDR_e'))
+        print(f'push!(Main.{self.name}, Main.pDR_e)')
+        Main.eval(
+            f'push!(Main.{self.name}, Main.pDR_e)'
+        )
+
+
 def states(state_list):
     """ Set states in the graph
 
@@ -45,7 +129,7 @@ def states(state_list):
     return graph_states
 
 
-def createFromLists(ids, state_counts):
+def statesFromLists(ids, state_counts):
     """ Set states in the graph
 
     ids -- a list of numbers identifying each node
@@ -57,53 +141,62 @@ def createFromLists(ids, state_counts):
     return graph_states
 
 
-def createVector(type: str, name: str = None):
-    ''' Create a new Julia vector of a given type
+def consequences(id, probabilities):
+    ''' Set values for each outcome on a value node
 
-    type: str -- The Julia type the vector contains
-    name: str -- Name of the vector in the Julia main namespace
+    id -- The id of the node
+    probabilities -- A Probabilies-object describing the
     '''
 
-    if name is None:
-        # Generate a unique 9 character name
-        name = 'n'+uuid.uuid4().hex[:8]
-        print(name)
-
-    print(f'{name} = Vector{{{type}}}()')
-    Main.eval(f'{name} = Vector{{{type}}}()')
-    return getattr(Main, name)
-
-
-def pushToVector(vec, element):
-    ''' Push and element to a given vector
-
-    vec -- the vector
-    element -- the new element
-    '''
-
-    Main.pyDecisionProgramming_v = vec
-    Main.pyDecisionProgramming_e = element
-    Main.eval(
-        'push!(Main.pyDecisionProgramming_v, Main.pyDecisionProgramming_e)'
-    )
-    return Main.pyDecisionProgramming_v
+    return jdp.Probabilities(id, probabilities)
 
 
 def chanceNode(id, nodes):
     ''' Create a chance node at given location in the graph
 
     id -- The id of the node
-    nodes -- A vector of connected nodes
+    nodes -- List of nodes connected to this node
     '''
 
-    return jdp.ChanceNode(id, nodes)
+    if isinstance(nodes, Vector):
+        assert(nodes.type == 'Node')
+
+        return Main.eval(f'ChanceNode({id}, Main.{nodes.name})')
+
+    else:
+        # Try with a python object, Julia will type-check
+        return jdp.ChanceNode(id, nodes)
 
 
-def probabilities(id, probabilities):
-    ''' Create a chance node at given location in the graph
+def valueNode(id, nodes):
+    ''' Create a value node at given location in the graph
 
     id -- The id of the node
-    nodes -- A vector of connected nodes
+    nodes -- List of nodes connected to this node
     '''
 
-    return jdp.Probabilities(id, probabilities)
+    if isinstance(nodes, Vector):
+        assert(nodes.type == 'Node')
+
+        return Main.eval(f'ValueNode({id}, Main.{nodes.name})')
+
+    else:
+        # Try with a python object, Julia will type-check
+        return jdp.ValueNode(id, nodes)
+
+
+def decisionNode(id, nodes):
+    ''' Create a decision node at given location in the graph
+
+    id -- The id of the node
+    nodes -- List of nodes connected to this node
+    '''
+
+    if isinstance(nodes, Vector):
+        assert(nodes.type == 'Node')
+
+        return Main.eval(f'DecisionNode({id}, Main.{nodes.name})')
+
+    else:
+        # Try with a python object, Julia will type-check
+        return jdp.DecisionNode(id, nodes)
