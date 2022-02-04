9f40416764d923e0935a029f01b42fe7128d8da7
Rantaharju Jarno <jarno.rantaharju@aalto.fi>
2022-02-04 14:08:36 +0200

    Move installation script to scripts/

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setup(
    author='Jarno Rantaharju',
    author_email='jarno.rantaharju@aalto.fi',
    packages=['pyDecisionProgramming'],
-   scripts=['pyDecisionProgramming/pdp_setup_julia.jl'],
+   scripts=['scripts/pdp_setup_julia.jl'],
    requires=['julia']
 )
