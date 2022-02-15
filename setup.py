aab3a13e8b87c02c9dd9e14b62f3c0a1b71bb1f9
Rantaharju Jarno <jarno.rantaharju@aalto.fi>
2022-02-15 16:28:59 +0200

    Add requirements

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
 from setuptools import setup
+from os.path import join, dirname
+
+requirementstxt = join(dirname(__file__), "requirements.txt")
+requirements = [line.strip() for line in open(requirementstxt, "r") if line.strip()]
 
 setup(
    name='pyDecisionProgramming',
@@ -8,5 +12,5 @@ setup(
    author_email='jarno.rantaharju@aalto.fi',
    packages=['pyDecisionProgramming'],
    scripts=['scripts/pdp_setup_julia.jl'],
-   requires=['julia']
+   requires=requirements
 )
