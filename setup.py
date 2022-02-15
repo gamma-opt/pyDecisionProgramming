a44eecff610c3386dcc146507cf6adbe11302010
Rantaharju Jarno <jarno.rantaharju@aalto.fi>
2022-02-15 16:48:39 +0200

    include the readme as the long description

--- setup.py
+++ setup.py
@@ -1,13 +1,21 @@
 from setuptools import setup
 from os.path import join, dirname
 
+# Load requirements from requirements.txt
 requirementstxt = join(dirname(__file__), "requirements.txt")
-requirements = [line.strip() for line in open(requirementstxt, "r") if line.strip()]
+with open(requirementstxt, "r") as requirements_file:
+    requirements = [line.strip() for line in requirements_file if line.strip()]
+
+# Load the README file as the long description
+readme = join(dirname(__file__), "README.md")
+with open(readme, "r") as readme_file:
+    long_description = readme_file.read()
 
 setup(
    name='pyDecisionProgramming',
    version='1.0',
    description='Python interface for DecisionProgramming.jl',
+   long_description=long_description,
    author='Jarno Rantaharju',
    author_email='jarno.rantaharju@aalto.fi',
    license="MIT",
